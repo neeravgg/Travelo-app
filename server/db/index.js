400d3cc931f401b18ef9d3787b8d841640192f86
Neerav Garg <neeravgarg366@gmail.com>
2022-11-18 20:17:13 +0530

    update

---
+++ server/db/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
+
+const db = mongoose.connection;
+
+module.exports = db;
